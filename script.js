e6b28db97605f76f8dfc2129da6de36f98a8791d
livvinha <livia.limeres.silva@escola.pr.gov.br>
2025-08-15 10:45:17 -0300

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+
+botaoMostraPalavras.addEventListener("click", botaopalavrachave); 
+
+function mostraPalavrasChave() { 
+  alert("fui clicado!"); 
+} 
