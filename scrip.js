80391e3524d0dc88f099b909496d089211dda2a6
livvinha <livia.limeres.silva@escola.pr.gov.br>
2025-08-29 12:29:24 -0300

    nfgjjf

--- scrip.js
+++ scrip.js
@@ -7,35 +7,5 @@ function mostraPalavrasChave() {
  const texto = document.querySelector("#entrada").value;
 const campoResultado = document.querySelector("#resultado");
 const palavras = texto.split(" ");
-campoResultado.textContent = texto;
-}
-
-function processaTexto(texto) {
-  let palavras = texto.split(/\P{L}+/u);
-
-  const frequencias = contaFrequencias(palavras);
-
-  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
-
-  function ordenaPalavra(p1, p2) {
-    return frequencias[p2] - frequencias[p1];
-  }
-
-  return ordenadas.slice(0, 10);
-}
-
-function contaFrequencias(palavras) {
-  let frequencias = {};
-
-  for (let i of palavras) {
-    frequencias[i] = 0;
-
-    for (let j of palavras) {
-      if (i == j) {
-        frequencias[i]++;
-      }
-    }
-  }
-
-  return frequencias;
+campoResultado.textContent = palavras.join(", ");
 }
