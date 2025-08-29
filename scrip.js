d1450a39af8e3b7081a2c5e1bf0f97212560c914
livvinha <livia.limeres.silva@escola.pr.gov.br>
2025-08-29 12:11:39 -0300

    bgf

--- scrip.js
+++ scrip.js
@@ -1,8 +1,41 @@
 
-const botao = document.querySelector("#botao"); 
+const botao = document.querySelector("#botao");
 
-botao.addEventListener("click", mostraPalavrasChave); 
+botao.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave() { 
-  alert("clicado!"); 
-} 
+function mostraPalavrasChave() {
+ const texto = document.querySelector("#entrada").value;
+const campoResultado = document.querySelector("#resultado");
+const palavras = texto.split(" ");
+campoResultado.textContent = texto;
+}
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  const frequencias = contaFrequencias(palavras);
+
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+  }
+
+  return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
+  let frequencias = {};
+
+  for (let i of palavras) {
+    frequencias[i] = 0;
+
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[i]++;
+      }
+    }
+  }
+
+  return frequencias;
+}
